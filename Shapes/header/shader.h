9750169a2165e014d99b8dc7aec0bde70a461154
BrianHwang888 <bh1531@nyu.edu>
2021-04-08 17:52:35 -0400

    added compiling shader functions

--- Shapes/header/shader.h
+++ Shapes/header/shader.h
@@ -1,32 +1,35 @@
 #ifndef __SHADER_H__
 #define	__SHADER_H__
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<glad/glad.h>
-#include<GLFW/glfw3.h>
-#include<glm/glm.hpp>
+#include"resource_manager.h"
 
 class shader_program {
 public:
 	GLuint ID;
 	
-	shader_program shader_program(const char* vertex_path, const char* fragment_path);
+	shader_program(const char* vertex_path, const char* fragment_path);
+
+
 	void use();
+	
+	/*----- set primitive data -----*/
 	void set_bool(const char* name, bool value);
-	void set_uniform_int(const char* name, GLint& value);
-	void set_uniform_float(const char* name, GLfloat& value);
-	void set_uniform_double(const char* name, GLdouble &value);
+	void set_int(const char* name, GLint& value);
+	void set_float(const char* name, GLfloat& value);
+	void set_double(const char* name, GLdouble &value);
 
 	/*----- set uniform vectors -----*/
-	void set_uniform_vec2(const char* name, const glm::vec2 &value);
-	void set_uniform_vec3(const char* name, const glm::vec3 &value);
+	void set_vec2(const char* name, const glm::vec2 &value);
+	void set_vec3(const char* name, const glm::vec3 &value);
 	void set_uniform_vec4(const char* name, const glm::vec4 &value);
 
 	/*----- set uniform matrices ----*/
 	void set_uniform_mat2(const char* name, const glm::mat2 &value);
 	void set_uniform_mat3(const char* name, const glm::mat3 &value);
 	void set_uniform_mat4(const char* name, const glm::mat4 &value);
+
+private:
+	GLuint* compile_shader(const char** shader_paths);
 };
 
 #endif // ! __SHADER_H__
