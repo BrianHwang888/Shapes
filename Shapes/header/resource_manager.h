673a44dfd060b7b178eabfe491df01868b032556
BrianHwang888 <bh1531@nyu.edu>
2021-04-08 17:51:25 -0400

    added read_file function and changed includes

--- Shapes/header/resource_manager.h
+++ Shapes/header/resource_manager.h
@@ -1,6 +1,11 @@
 #ifndef __RESOURCE_MANAGER_H__
 #define __RESOURCE_MANAGER_H__
 
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<glm/glm.hpp>
+#include<stdio.h>
+#include<stdlib.h>
 
-
+const char* read_file(const char* file_path);
 #endif // !__RESOURCE_MANAGER_H__
